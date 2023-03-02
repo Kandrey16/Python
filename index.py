b528709af5f59bdd29d395dcea71c87b8277e2e7
Andrey Kurdelov <isip_a.n.kurdelov@mpt.ru>
2023-03-02 12:48:51 +0300

    FourYear

--- index.py
+++ index.py
@@ -1,13 +1,28 @@
 year = int(input('Введите год: '))
 int_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 result = 0
-if(year % 4 == 0):
+if ((year % 4 == 0) and not (year % 100 == 0)) or (year % 400 == 0): 
     int_list[1] = 29
     for x in range(12):
         for y in range(int_list[x]+1):
             result +=  y // 10 + y % 10
-elif(year % 4 !=0):
+else:
     for x in range(12):
         for y in range(int_list[x]+1):
             result += y // 10 + y % 10
 print(result)
+
+
+# year = int(input('Введите год: '))
+# int_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# result = 0
+# if(year % 4 == 0):
+#     int_list[1] = 29
+#     for x in range(12):
+#         for y in range(int_list[x]+1):
+#             result +=  y // 10 + y % 10
+# elif(year % 4 !=0):
+#     for x in range(12):
+#         for y in range(int_list[x]+1):
+#             result += y // 10 + y % 10
+# print(result)
